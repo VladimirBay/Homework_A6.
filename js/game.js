cc2f6a7e398c1787c305395068cd316c5bf26fbf
VladimirBay <62301791+VladimirBay@users.noreply.github.com>
2020-04-11 14:30:30 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,60 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let fails = 0;
+let firstHitTime = 0;
+
+function getTimestamp() {
+  let d = new Date();
+  return d.getTime();
+}
+
+function randomDivId() {
+  let d = Math.floor(Math.random() * 6) + 1;
+  let n = Math.floor(Math.random() * 6) + 1;
+  return `#slot-${d}${n}`;
+}
+
+function round() {
+  $('.target').removeClass('target'); 
+  $('.miss').removeClass('miss'); 
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass('target')
+  $(divSelector).text(hits + 1)
+
+  if (hits === 1) { firstHitTime = getTimestamp(); } 
+  if (hits === maxHits) { endGame(); } 
+}
+
+function endGame() {
+  $('.game-field').hide();
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $('#total-time-played').text(totalPlayedSeconds);
+  $('#totalScores').text(hits - fails);
+
+  $('#win-message').removeClass("d-none");
+}
+
+function handleClick(event) {
+  let target = $(event.target)
+  if (target.hasClass('target')) {
+    hits = hits + 1;
+    target.text('');
+    round();
+  } else { $(event.target).addClass('miss'); } 
+}
+
+function init() {
+    round();
+
+    $('.game-field').click(handleClick);
+    $('#button-reload').click(function() {
+      location.reload();
+  });
+}
+
+$(document).ready(init);
